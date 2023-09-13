850e133cf8bb74e2bdf8e1a8a1f40db0e7dc1267
Colin Ogoo <ogoo.colin@gmail.com>
2023-09-13 18:54:04 +0000

    chore: update images

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -15,12 +15,12 @@
 						<div class="flex flex-shrink-0 items-center">
 							<img
 								class="block h-8 w-auto lg:hidden"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								src="/img/logos/cf-white.png"
 								alt="Your Company"
 							/>
 							<img
 								class="hidden h-8 w-auto lg:block"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								src="/img/logos/cf-white.png"
 								alt="Your Company"
 							/>
 						</div>
@@ -83,11 +83,7 @@
 								>
 									<span class="absolute -inset-1.5" />
 									<span class="sr-only">Open user menu</span>
-									<img
-										class="h-8 w-8 rounded-full"
-										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-										alt=""
-									/>
+									<img class="h-8 w-8 rounded-full" src="/img/avatar.png" alt="" />
 								</button>
 							</div>
 
