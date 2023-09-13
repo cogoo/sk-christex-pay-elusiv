eb74b6772cdba248563f5890c38eec85bc38a40a
Colin Ogoo <ogoo.colin@gmail.com>
2023-09-13 20:07:07 +0000

    feat(app): send button on dashboard page

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -27,7 +27,7 @@
 						<div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
 							<!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
 							<a
-								href="#"
+								href="/app/dashboard"
 								class="border-indigo-500 text-gray-900 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
 								aria-current="page">Dashboard</a
 							>
--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -3,6 +3,18 @@
 		<header class="pb-4 pt-6 sm:pb-6">
 			<div class="mx-auto flex max-w-7xl flex-wrap items-center gap-6 sm:flex-nowrap px-6">
 				<h1 class="text-base font-semibold leading-7 text-gray-900">Dashboard</h1>
+
+				<a
+					href="#"
+					class="ml-auto flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+				>
+					<svg class="-ml-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+						<path
+							d="M10.75 6.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"
+						/>
+					</svg>
+					Send
+				</a>
 			</div>
 		</header>
 
